afce660667769eaaa30ebcf87fd9bb4c44351dc3
danield001 <30894506+danield001@users.noreply.github.com>
2023-11-21 12:32:41 +1030

    changes to js files

---
+++ library/shapes.js
@@ -0,0 +1,14 @@
+class shape {
+    constructor(){
+        this.color = "";
+    }
+    setColor(colorVar){
+        this.color = colorVar;
+    }
+};
+
+class triangle extends shape {
+    render() {
+        return `<polygon points="150, 18 244, 182 56, 182" fill="${answers.logoColor"/>`
+    }
+}
